0607b31f4a2df71964179ef943e3a58e96bfafb9
Gaby Fersan <gabyfersan@Gabys-iMac.local>
2024-07-01 23:05:19 +0200

    First commit for MUI GU

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-// Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-// Bootstrap Bundle JS
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+const theme = createTheme({
+  typography: {
+    fontFamily: "Consolas, Courier, monospace",
+  },
+});
+
+ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
