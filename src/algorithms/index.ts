f29f0fa31655b3352afa0b024a270d5d270ebfbd
Gaby Fersan <gabyfersan@Gabys-iMac.local>
2024-07-08 01:23:19 +0200

    Clean algoritm

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -14,33 +14,46 @@ const maximumOddSum = (numbers: number[]): number => {
     }
   }
 
-  console.log("maximumOddSum", maxOdd === null || maxEven === null ? -1 : maxOdd + maxEven;)
+  console.log(
+    "maximumOddSum",
+    maxOdd === null || maxEven === null ? -1 : maxOdd + maxEven
+  );
   return maxOdd === null || maxEven === null ? -1 : maxOdd + maxEven;
   //return [maxOdd, maxEven, maxOdd + maxEven];
 };
 
 export { maximumOddSum, removeIdenticalLetters };
 
-function generateRandomString(length) {
+function generateRandomString(length: number) {
   const characters = "abcdefghijklmnopqrstuvwxyz";
   let result = "";
   const charactersLength = characters.length;
 
   for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    result += characters.charAt(
+      Math.floor(Math.random() * charactersLength)
+    );
   }
 
   return result;
 }
 
-
-function generateArray(length) {
-  if (typeof length !== 'number' || length <= 0 || !Number.isInteger(length)) {
-    throw new Error('The input parameter must be a positive integer.');
+function generateArray(length: number) {
+  if (
+    typeof length !== "number" ||
+    length <= 0 ||
+    !Number.isInteger(length)
+  ) {
+    throw new Error(
+      "The input parameter must be a positive integer."
+    );
   }
 
-  const maxVal = 1500000.;
-  const array = Array.from({ length }, () => Math.floor(Math.random() * maxVal) + 1);
+  const maxVal = 1500000;
+  const array = Array.from(
+    { length },
+    () => Math.floor(Math.random() * maxVal) + 1
+  );
 
   return array;
 }
@@ -49,8 +62,7 @@ function generateArray(length) {
 const randomString = generateRandomString(10); // Generates a random string of length 10
 console.log(randomString);
 
-
-function removeIdenticalLetters2(s) {
+function removeIdenticalLetters2(s: string) {
   const pattern = /(.)\1\1\1/g;
   return s.replace(pattern, (match) => match.slice(0, 3));
 }
