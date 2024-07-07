851425b55f4824dcc63289fdd432bfd93035f4ab
gabyfersan <44554967+gabyfersan@users.noreply.github.com>
2024-07-07 11:14:20 +0200

    More on algorithm

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -1,5 +1,5 @@
 const removeIdenticalLetters = (str: string): string => {
-  return '';
+  return "";
 };
 
 const maximumOddSum = (numbers: number[]): number => {
@@ -14,8 +14,43 @@ const maximumOddSum = (numbers: number[]): number => {
     }
   }
 
+  console.log("maximumOddSum", maxOdd === null || maxEven === null ? -1 : maxOdd + maxEven;)
   return maxOdd === null || maxEven === null ? -1 : maxOdd + maxEven;
   //return [maxOdd, maxEven, maxOdd + maxEven];
 };
 
-export { removeIdenticalLetters, maximumOddSum };
+export { maximumOddSum, removeIdenticalLetters };
+
+function generateRandomString(length) {
+  const characters = "abcdefghijklmnopqrstuvwxyz";
+  let result = "";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+
+function generateArray(length) {
+  if (typeof length !== 'number' || length <= 0 || !Number.isInteger(length)) {
+    throw new Error('The input parameter must be a positive integer.');
+  }
+
+  const maxVal = 1500000.;
+  const array = Array.from({ length }, () => Math.floor(Math.random() * maxVal) + 1);
+
+  return array;
+}
+
+// Example usage:
+const randomString = generateRandomString(10); // Generates a random string of length 10
+console.log(randomString);
+
+
+function removeIdenticalLetters2(s) {
+  const pattern = /(.)\1\1\1/g;
+  return s.replace(pattern, (match) => match.slice(0, 3));
+}
